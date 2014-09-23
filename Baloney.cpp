60ba04dfb5427e5e133a5806f62eaa9fe00f810d
Daniel Porter <porterdj1@gcc.edu>
2014-09-23 19:47:55 -0400

    Hopefully this works

--- Baloney.cpp
+++ Baloney.cpp
@@ -1,10 +1,46 @@
 // Baloney.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "Card.h"
+#include "Card.cpp"
+#include <iostream>
+#include <vector>
 
+class pile {
+public: 
+	void handout(int players);
+	void shuffle(Value deck[]);
+private:
+	std::vector<Value> pile;
+	Value deck[];
+};
 
-int _tmain(int argc, _TCHAR* argv[])
+class player {
+public: 
+	void displayHand();
+	void playGame();
+	void winGame();
+private:
+	std::vector<int> hand;
+};
+
+class game{
+public:
+	void baloney();
+	Value handOutCards();
+private:
+	Value playCard;
+};
+
+class AI{
+public:
+	void baloney();
+	void playGame();
+	void winGame();
+private:
+	std::vector<int> hand;
+};
+int main()
 {
 	return 0;
 }
