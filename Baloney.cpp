82e858bb2021ba48af961171d971e0d2a0d9aa68
Daniel Porter <porterdj1@gcc.edu>
2014-09-24 22:51:40 -0400

    Major Update
    Sync Now!

--- Baloney.cpp
+++ Baloney.cpp
@@ -9,21 +9,14 @@
 #include <ctime>
 #include <random>
 
-class pile {
-public: 
-	void deal(player, AI);
-	void createDeck();
-	std::vector<int> showTable();
-private:
-	std::vector<int> table;
-	std::vector<int> deck;
-};
+
 
 class player {
 public: 
 	void displayHand();
 	void playGame();
 	void winGame();
+	void pushHand(int i);
 	std::vector<int> showHand();
 private:
 	std::vector<int> hand;
@@ -31,23 +24,42 @@ private:
 
 class game{
 public:
+	game(Value playCard=two);
 	void baloney();
 	Value handOutCards();
+	int seeCard();
+	void changeCard();
+	std::vector<int> showVec();
+	void pushVec(int i);
 private:
 	Value playCard;
+	std::vector<int> currentVec;
 };
 
 class AI{
 public:
 	std::vector<int> showHand();
 	void baloney();
-	void playGame();
+	void playGame(game&);
 	void winGame();
+	void pushHand(int i);
 private:
 	std::vector<int> hand;
 };
+
+class pile {
+public: 
+	void createDeck();
+	void deal(player&, AI&);
+	std::vector<int> showTable();
+private:
+	std::vector<int> table;
+	std::vector<int> deck;
+};
+
 int main()
 {
+	game bubpip;
 	pile fart;
 	player Bob;
 	AI Alison;
@@ -68,24 +80,23 @@ void pile::createDeck()
 	std::random_shuffle(pile::deck.begin(), pile::deck.end());
 }
 
-void pile::deal(player player, AI AI)
+void pile::deal(player& player, AI& AI)
 {
 	for(int i=0; i<52; i++)
 	{
 		 if(i<20)
 		 {
-			player.player::showHand().push_back(deck.at(i));
+			 player.pushHand(deck.at(i));
 		 }
 		 else if(i>=20 && i<40)
 		 {
-			AI.AI::showHand().push_back(deck.at(i));
+			AI.pushHand(deck.at(i));
 		 }
 		 else
 		 {
 			table.push_back(deck.at(i));
 		 }
 	}
-	
 }
 
 void player::displayHand()
@@ -94,6 +105,41 @@ void player::displayHand()
 	{
 		std::cout << hand.at(i) << "  ";
 	}
+}
+
+int game::seeCard()
+{
+	return playCard;
+}
+
+void game::changeCard()
+{
+	switch(playCard) {
+	case ace: playCard = two;
+	case two: playCard = three;
+	case three: playCard = four;
+	case four: playCard = five;
+	case five: playCard = six;
+	case six:  playCard = seven;
+	case seven: playCard = eight;
+	case eight: playCard = nine;
+	case nine: playCard = ten;
+	case ten: playCard = jack;
+	case jack: playCard = queen;
+	case queen: playCard = king;
+	case king: playCard = ace;
+	default: std::cout << "WTF are you doing";
+	}
+}
+
+void player::pushHand(int i)
+{
+	player::hand.push_back(i);
+}
+
+void AI::pushHand(int i)
+{
+	AI::hand.push_back(i);
 }
 
 std::vector<int> player::showHand()
@@ -110,3 +156,46 @@ std::vector<int> pile::showTable()
 {
 	return pile::table;
 };
+
+void AI::playGame(game& bubpip)
+{
+	for(int i=0; i<hand.size(); i++)
+	{
+		if(hand.at(i) == bubpip.seeCard)
+		{
+			bubpip.pushVec(hand.at(i));
+			hand.erase(hand.begin() + i);
+		}
+	}
+	if(bubpip.showVec().size() == 0)
+	{
+		bubpip.pushVec(hand.at(0));
+		hand.erase(hand.begin());
+	}
+}
+
+std::vector<int> game::showVec()
+{
+	return game::currentVec;
+};
+
+void game::pushVec(int i)
+{
+	game::currentVec.push_back(i);
+}
+
+void player::winGame()
+{
+	if(hand.size() == 0)
+	{
+		std::cout << "You win the game!";
+	}
+}
+
+void AI::winGame()
+{
+	if(hand.size() == 0)
+	{
+		std::cout << "You lose the game!";
+	}
+}
