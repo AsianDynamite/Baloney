ec34149d4f90b35bf703891e9db7d5c2b3c62419
Daniel Porter <porterdj1@gcc.edu>
2014-09-25 21:41:25 -0400

    DONE!!!
    Game is done yo

--- Baloney.cpp
+++ Baloney.cpp
@@ -1,5 +1,10 @@
-// Baloney.cpp : Defines the entry point for the console application.
-//
+/*
+	Author:	Rick Magill, Mitchell Buzard, Daniel Potter
+	Course:	COMP 220, Computer Programming II
+	Date:	9/25/14
+	Description:	This program allows the user to play a game of 
+	baloney against an AI player. 
+*/
 
 #include <iostream>
 #include <vector>
@@ -8,10 +13,10 @@
 #include <random>
 #include <string>
 
-
-enum Suit {clubs, diamonds, hearts, spades};
+//This is the enumeration for the cards 
 enum Value {ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king};
 
+//This is a class for the AI player
 class AI{
 public:
 	std::vector<int> showHand();
@@ -19,12 +24,12 @@ public:
 	bool winGame();
 	void pushHand(int i);
 	void deleteVec(int);
-	void displayHand(); 
 	std::string AI::valueName(int i);
 private:
 	std::vector<int> hand;
 };
 
+//This is a class for the user
 class player {
 public: 
 	void displayHand();
@@ -42,57 +47,67 @@ private:
 	std::vector<int> currentVec;
 };
 
+//This is a class for the handling of the cards 
 class pile {
 public: 
 	void createDeck();
 	void deal(player&, AI&);
 	void takeVec(std::vector<int>);
 	std::vector<int> showTable();
+	void throwTable(pile&);
 private:
 	std::vector<int> table;
 	std::vector<int> deck;
 };
 
+//This is a class for playing the game
 class game{
 public:
 	game();
 	Value changeCard(Value);
 	void pushVec(int i);
+
+	//This is the game
 	void baloney(player&, game&, AI&, pile&);
 	int seeCard();
 	int stringToVal(std::string);
 	Value handOutCards();
 	std::string valueName(int i);
 	std::vector<int> showVec();
-	~game();
+	void deleteVec();
+ 	~game();
 private:
 	Value playCard;
 	std::vector<int> currentVec;
 };
 
+//This organized the classes 
 int main()
 {
 	game bubpip;
-	pile fart;
+	pile cards;
 	player Bob;
 	AI Alison;
-	fart.createDeck();
-	fart.deal(Bob, Alison);
-	bubpip.baloney(Bob, bubpip, Alison, fart);
+	cards.createDeck();
+	cards.deal(Bob, Alison);
+	bubpip.baloney(Bob, bubpip, Alison, cards);
 	return 0;
 }
 
+//This creates the deck
 void pile::createDeck()
 {
 	std::srand(time(NULL));
 	for(int i=0; i<52; i++)
 	{
+		//Fills the deck with card enums 
 		pile::deck.push_back(Value(i%13));
 	}
-
+	//This shuffles the deck 
 	std::random_shuffle(pile::deck.begin(), pile::deck.end());
 }
 
+//This deals out the cards to the player, AI, and table 
 void pile::deal(player& player, AI& AI)
 {
 	for(int i=0; i<52; i++)
@@ -112,6 +127,7 @@ void pile::deal(player& player, AI& AI)
 	}
 }
 
+//This displays the player's hand
 void player::displayHand()
 {
 	std:: cout << "Your cards: " << std::endl;
@@ -122,24 +138,13 @@ void player::displayHand()
 	std::cout << std:: endl;
 }
 
-void AI::displayHand()
-{
-	std:: cout << "AI's cards: " << std::endl;
-	for(int i=0; i<hand.size(); i++)
-	{
-		std::cout << valueName(hand.at(i)) << std::endl;
-	}
-	std::cout << std:: endl;
-
-
-
-
-	}
+//This returns the current required card
 int game::seeCard()
 {
 	return playCard;
 }
 
+//This converts the enum into a string for the player
 std::string player::valueName(int i)
 {
 	switch(i) {
@@ -159,6 +164,8 @@ std::string player::valueName(int i)
 	default: throw std::out_of_range("Face value out of range");
 	}
 }
+
+//This converts the enum in a string for the AI
 std::string AI::valueName(int i)
 {
 	switch(i) {
@@ -179,6 +186,7 @@ std::string AI::valueName(int i)
 	}
 }
 
+//This converts the enum into a string for the game
 std::string game::valueName(int i)
 {
 	switch(i) {
@@ -199,6 +207,7 @@ std::string game::valueName(int i)
 	}
 }
 
+//This advances the current required card for the game
 Value game::changeCard(Value currentCard)
 {
 	switch(currentCard) {
@@ -219,55 +228,67 @@ Value game::changeCard(Value currentCard)
 	}
 }
 
+//This takes an integer and pushes it into the player's hand
 void player::pushHand(int i)
 {
 	player::hand.push_back(i);
 }
 
+//This takes an integer and pushes it into the AI's hand
 void AI::pushHand(int i)
 {
 	AI::hand.push_back(i);
 }
 
+//This returns the play's hand
 std::vector<int> player::showHand()
 {
 	return player::hand;
 };
 
+//This returns the AI's hand
 std::vector<int> AI::showHand()
 {
 	return AI::hand;
 };
 
+//This returns the cards that are on the table 
 std::vector<int> pile::showTable()
 {
 	return pile::table;
 }
 
+//This removes a card from the AI's hand
 void AI::deleteVec(int i)
 {
 	hand.erase(hand.begin() +i);
 }
+
+//This returns the vector for the AI
 std::vector<int> game::showVec()
 {
 	return game::currentVec;
 }
 
+//This function pushes a card onto the table
 void game::pushVec(int i)
 {
 	game::currentVec.push_back(i);
 }
 
+//This returns what the player is playing 
 std::vector<int> player::showVec()
 {
 	return currentVec;
 }
 
+//This clears the cards that the player is playing 
 void player::deleteVec()
 {
 	currentVec.clear();
 }
 
+//This tells the player when the have won
 bool player::winGame()
 {
 	if(hand.size() == 0)
@@ -278,6 +299,7 @@ bool player::winGame()
 	return true;
 }
 
+//This tells the player when they have lost 
 bool AI::winGame()
 {
 	if(hand.size() == 0)
@@ -288,11 +310,13 @@ bool AI::winGame()
 	return true;
 }
 
+//This pushes cards into the player's current played cards 
 void player::pushVec(int i)
 {
 	currentVec.push_back(i);
 }
 
+//This pushes cards onto the table 
 void pile::takeVec(std::vector<int> A)
 {
 	for(int i=0; i < A.size(); i++)
@@ -301,26 +325,31 @@ void pile::takeVec(std::vector<int> A)
 	}
 }
 
-std::vector<int> AI::showAndDelete(AI& AI)
+//This deletes the cards played by the AI
+void game::deleteVec()
 {
-	std::vector<int> A = AI.hand;
-	AI.hand.clear();
-	return A;
+	currentVec.clear();
 }
 
+//This clears cards from the table
+void pile::throwTable(pile& pile)
+{
+	pile.table.clear();
+}
+
+//This sets the intial card to be played
 game::game()
 {
 	playCard = two;
 }
+
+//This is a destructor for the game
 game::~game()
 {	
 	
 }
 
-
-
-
-
+//This changes the player's input into value enumeration
 int game::stringToVal(std::string S)
 {
 	for(int i=0; i < S.size(); i++)
@@ -382,6 +411,7 @@ int game::stringToVal(std::string S)
 	return 666;
 }
 
+//Checks to see if the player has the card that they wish to play 
 int player::checkHand(int i)
 {
 	
@@ -398,26 +428,30 @@ int player::checkHand(int i)
 	return 755;
 }
 
+//This plays the game
 void game::baloney(player& player, game& bubpip, AI& AI, pile& pile)
 {
 	
 	std::string playerCard;
 	std::string playerChoice;
-	int checkVec = 0;
+	int checkVec = 0, blah = 0;
+
+	//This do-while plays the game until someone wins
 	do
 	{
+
+	//This do-while lets the player play 
 	do
 	{
 		player.displayHand();
-		AI.displayHand(); 
-		std::cout << "You have to play a " << valueName(playCard) << "." << " Only input strings!!!!";
+		std::cout << "You have to play a " << valueName(bubpip.playCard) << "." << " Only input strings!!!!";
 		std::cin >> playerCard;
 		checkVec = player.checkHand(stringToVal(playerCard));
+		//This while checks if the card is good to be played
 	}while (stringToVal(playerCard)<0 || stringToVal(playerCard)>12 || checkVec == 755);
-	
-	playCard=changeCard(playCard);
 
-	if(player.showVec().at(0)==playCard)
+	//This is the AI choosing to call baloney on the player
+	if(player.showVec().at(0)==bubpip.playCard)
 	{
 		if(rand()%4==3)
 		{
@@ -431,44 +465,89 @@ void game::baloney(player& player, game& bubpip, AI& AI, pile& pile)
 		std::cout << std::endl << "The AI called Baloney!" << std::endl;
 	}
 
-	playCard=changeCard(playCard);
+	//This changes the required card
+	bubpip.playCard=changeCard(bubpip.playCard);
 
+	//This is the AI playing
 	for(int i=0; i<AI.showHand().size(); i++)
 	{
+		//This is if the AI has the card
 		if(AI.showHand().at(i) == bubpip.seeCard())
 		{
 			bubpip.pushVec(AI.showHand().at(i));
 			AI.deleteVec(i);
-			std::cout << "The AI played a " << playCard << std::endl;
+			std::cout << "The AI played a " << valueName(bubpip.playCard) << std::endl;
 		}
 	}
+	//This is if the AI doesn't have the card
 	if(bubpip.showVec().size() == 0)
 	{
 		bubpip.pushVec(AI.showHand().at(0));
 		AI.deleteVec(0);
-		std::cout << "The AI played a " << playCard << std::endl;
-
+		std::cout << "The AI played a " << valueName(bubpip.playCard) << std::endl;
 	}
+
+	//This is the player calling baloney on the AI
 	do
 	{
 	std::cout << std::endl << "Would you like to call Baloney?(yes/no)" << std::endl;
 	std:: cin >> playerChoice;
+
+	//Continue to ask until they say yes or no 
 	}while(playerChoice!="yes" && playerChoice!="no");
 
 	if(playerChoice == "yes")
 	{
-		if(AI.showHand().at(0)==playCard)
-			for(int i=0; i < pile.showTable().size(); i++)
-			{
-				pile.takeVec(AI.showAndDelete(AI));
-			}
-		else
+		//This checks if the player's guess was right
+		if(bubpip.showVec().at(0)==bubpip.playCard)
 		{
 			for(int i=0; i < pile.showTable().size(); i++)
 			{
-				pile.takeVec(AI.showAndDelete(AI));
+				if(i ==0)
+				{
+					for(int j=0; j<bubpip.currentVec.size(); j++)
+					{
+						player.pushHand(bubpip.currentVec.at(j)); 
+					}
+
+
+					}
+				player.pushHand(pile.showTable().at(i));
 			}
 		}
+
+		//This is if the player guessed wrong
+		else
+		{
+		
+			for(int i=0; i < pile.showTable().size(); i++)
+			{
+				if(i ==0)
+				{
+					for(int j=0; j<bubpip.currentVec.size(); j++)
+					{
+						AI.pushHand(bubpip.currentVec.at(j)); 
+					}
+
+
+					}}
+			for(int i=0; i < pile.showTable().size(); i++)
+		{	
+				AI.pushHand(pile.showTable().at(i));
+			}
+		//This emties the table after it has been distributed
+		pile.throwTable(pile);
+		}
 	}
+
+	//This increases the required card
+	bubpip.playCard=changeCard(bubpip.playCard);
+
+	//This dumps all of the vectors for the next turn
+	pile.takeVec(player.showVec());
+	player.deleteVec();
+	pile.takeVec(bubpip.currentVec);
+	bubpip.deleteVec();
 }while(AI.winGame() && player.winGame());
 }
+	
